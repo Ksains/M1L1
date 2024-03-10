c673850fb3a9f0f07153e00650ae1303aecbec3b
Ksains <158805541+Ksains@users.noreply.github.com>
2024-03-10 14:10:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(5):
+        await ctx.send(content)
+
+bot.run("MTIxMzgxMTI0MzE1NzI5NTEyNA.Gp3GAN.HqkpiuR0XkXaxRpHX5NrvRPEMV1fuj1HPG0hTU")
